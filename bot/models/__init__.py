22c170e74502f6b0a97169125d9c51bb0dd9f92d
Kulunchick <nikolikrasovski@gmail.com>
2023-04-18 22:42:02 +0300

    UPD: Added models

---
+++ bot/models/__init__.py
@@ -0,0 +1,16 @@
+from .base import Base
+from .answer import Answer
+from .question import Question
+from .event import Event
+from .user import User
+from .request import Request
+
+
+__all__ = [
+    "Base",
+    "User",
+    "Event",
+    "Request",
+    "Question",
+    "Answer"
+]
---
+++ bot/models/event.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from typing import List, TYPE_CHECKING
+
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+
+from bot.models.base import Base
+
+if TYPE_CHECKING:
+    from bot.models.request import Request
+    from bot.models.question import Question
+
+
+class Event(Base):
+    __tablename__ = "events"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str]
+    date: Mapped[datetime]
+
+    questions: Mapped[List["Question"]] = relationship(back_populates="event")
+    requests: Mapped[List["Request"]] = relationship(back_populates="event")
