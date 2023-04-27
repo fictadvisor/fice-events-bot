ca43b7dbb22ed1fbc02156b0dcf5f96f05ba15e4
Kulunchick <nikolikrasovski@gmail.com>
2023-04-28 00:06:17 +0300

    UPD: Changed ReplyKeyboard to InlineKeyboard

--- bot/keyboards/confirm.py
+++ bot/keyboards/confirm.py
@@ -1,11 +1,23 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from enum import Enum
+
+from aiogram.filters.callback_data import CallbackData
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 from bot.keyboards.buttons import YES, NO
 
-CONFIRM_KEYBOARD = ReplyKeyboardMarkup(
-    keyboard=[[
-        KeyboardButton(text=YES),
-        KeyboardButton(text=NO)
+
+class Select(str, Enum):
+    YES = "yes"
+    NO = "no"
+
+
+class ConfirmData(CallbackData, prefix="confirm"):
+    select: Select
+
+
+CONFIRM_KEYBOARD = InlineKeyboardMarkup(
+    inline_keyboard=[[
+        InlineKeyboardButton(text=YES, callback_data=ConfirmData(select=Select.YES).pack()),
+        InlineKeyboardButton(text=NO, callback_data=ConfirmData(select=Select.NO).pack())
     ]],
-    resize_keyboard=True
 )
